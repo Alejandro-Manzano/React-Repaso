7eea21f986073c453f84983c2339f2edc4dd50a1
Alejandro Manzano <alejandromanzano.laforga@gmail.com>
2023-07-20 11:58:28 +0200

    create context

--- react-course/src/pages/App/App.jsx
+++ react-course/src/pages/App/App.jsx
@@ -1,4 +1,5 @@
 import { useRoutes, BrowserRouter } from 'react-router-dom' //instalar react-router-dom
+import { ShoppingCartProvider } from '../../context'
 import Home from '../Home'
 import MyAccount from '../MyAccount'
 import MyOrder from '../MyOrder'
@@ -26,10 +27,12 @@ const AppRoutes = () => {
 const App = () => {
 
   return (
-    <BrowserRouter>
-      <AppRoutes></AppRoutes>
-      <NavBar></NavBar>
-    </BrowserRouter>
+    <ShoppingCartProvider>
+        <BrowserRouter>
+          <AppRoutes></AppRoutes>
+          <NavBar></NavBar>
+        </BrowserRouter>
+    </ShoppingCartProvider>
   )
 }
 
